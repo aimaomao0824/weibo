843f46b7d2369827522eac8c0743c0b036227ce0
Jeffrey <jeffrey@jeffreydemacbook-pro.local>
2016-09-26 23:54:30 +0800

    创建了人类

---
+++ weibo/weibo/perpson.h
@@ -0,0 +1,13 @@
+//
+//  perpson.h
+//  weibo
+//
+//  Created by Jeffrey on 16/9/26.
+//  Copyright © 2016年 深圳市满泰科技发展有限公司. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface perpson : NSObject
+
+@end
---
+++ weibo/weibo/perpson.m
@@ -0,0 +1,13 @@
+//
+//  perpson.m
+//  weibo
+//
+//  Created by Jeffrey on 16/9/26.
+//  Copyright © 2016年 深圳市满泰科技发展有限公司. All rights reserved.
+//
+
+#import "perpson.h"
+
+@implementation perpson
+
+@end
